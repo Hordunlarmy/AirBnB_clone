5d15b350348e486ed3d17c76d6c6c1ad65a25686
Hordunlarmy <Hordunlarmy@gmail.com>
2023-10-10 20:16:26 +0100

    Update BaseModel string representation format

--- models/base_model.py
+++ models/base_model.py
@@ -31,7 +31,7 @@ class BaseModel:
     def __str__(self):
         """Format string based on [<class name>] (<self.id>) <self.__dict__>"""
 
-        return f"{self.__class__.__name__} ({self.id}) {self.__dict__}"
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -39,6 +39,8 @@ class FileStorage:
         if os.path.isfile(FileStorage.__file_path):
             with open(FileStorage.__file_path) as f:
                 obj_dict = json.load(f)
+                return obj_dict
                 for key, value in obj_dict.items():
-                    class_name = obj_dict[key]["__class__"]
-                    FileStorage.__objects[key] = class_name(**value)
+                    class_name = value['__class__']
+                    obj_instance = eval(class_name)(**value)
+                    FileStorage.__objects[key] = obj_instance
