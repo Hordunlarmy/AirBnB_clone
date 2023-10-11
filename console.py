9dfc553abd61f36238bce158a4a633da890cc37f
Hordunlarmy <Hordunlarmy@gmail.com>
2023-10-11 21:59:55 +0100

    Add create, show, and destroy commands with error handling

--- console.py
+++ console.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 import cmd
+import models
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
     """Class definition"""
 
     prompt = "(hbnb) "
+
+    class_names = ["BaseModel", "User", "State", "City", "Amenity", "Place",
+                   "Review"]
 
     def do_quit(self, arg):
         """Exit the program"""
@@ -20,12 +25,70 @@ class HBNBCommand(cmd.Cmd):
         print("Quit command to exit the program\n")
 
     def default(self, line):
-        """Called on an unrecognized command"""
+        """Called on an empty command"""
         if not line:
             return
+
     def emptyline(self):
         """Do nothing on empty input line"""
         pass
+
+    def do_create(self, arg):
+        """
+         Creates a new instance of BaseModel, saves it (to the JSON file) and
+         prints the id
+        """
+        if arg is None or arg == "":
+            print("** class name missing **")
+        elif arg not in HBNBCommand.class_names:
+            print("** class doesn't exist **")
+        else:
+            new_obj = BaseModel()
+            new_obj.save()
+            print(new_obj.id)
+
+    def do_show(self, arg):
+        """
+        Prints the string representation of an instance based on the class
+        name and id
+        """
+        args = arg.split()
+
+        if arg is None or arg == "":
+            print("** class name missing **")
+        elif args[0] not in HBNBCommand.class_names:
+            print("** class doesn't exist **")
+        else:
+            if len(args) < 2:
+                print("** instance id missing **")
+            else:
+                key = "{}.{}".format(args[0], args[1])
+                if key not in models.storage.all():
+                    print("** no instance found **")
+                else:
+                    print(models.storage.all()[key])
+
+    def do_destroy(self, arg):
+        """
+        Deletes an instance based on the class name and id
+        (save the change into the JSON file)
+        """
+        args = arg.split()
+
+        if arg is None or arg == "":
+            print("** class name missing **")
+        elif args[0] not in HBNBCommand.class_names:
+            print("** class doesn't exist **")
+        else:
+            if len(args) < 2:
+                print("** instance id missing **")
+            else:
+                key = "{}.{}".format(args[0], args[1])
+                if key not in models.storage.all():
+                    print("** no instance found **")
+                else:
+                    del models.storage.all()[key]
+                    models.storage.save()
 
 
 if __name__ == "__main__":
