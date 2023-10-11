ac8aa76c207aad2e64a9786658411a59ff81d0b0
Hordunlarmy <Hordunlarmy@gmail.com>
2023-10-11 13:49:23 +0100

    Add console commands and handling for quit and EOF in HBNBCommand class

--- console.py
+++ console.py
@@ -3,7 +3,29 @@ import cmd
 
 
 class HBNBCommand(cmd.Cmd):
-    pass
+    """Class definition"""
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """Handle EOF (Ctrl+D) to exit"""
+        return True
+
+    def help_quit(self):
+        """Change help message for the quit command"""
+        print("Quit command to exit the program\n")
+
+    def default(self, line):
+        """Called on an unrecognized command"""
+        if not line:
+            return
+    def emptyline(self):
+        """Do nothing on empty input line"""
+        pass
 
 
 if __name__ == "__main__":
