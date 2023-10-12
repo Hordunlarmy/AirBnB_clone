34cf1a194103bc01794695bf29bbec5de322c0ca
Hordunlarmy <Hordunlarmy@gmail.com>
2023-10-12 09:01:27 +0100

    Update file storage to not use indentation when dumping JSON data.Replaces indentation with no indentation when dumping JSON data to the file

--- console.py
+++ console.py
@@ -90,6 +90,66 @@ class HBNBCommand(cmd.Cmd):
                     del models.storage.all()[key]
                     models.storage.save()
 
+    def do_all(self, arg):
+        """
+         Prints all string representation of all instances based or not on the
+         class name.
+        """
+
+        instance_rep = []
+        if arg:
+            if arg not in HBNBCommand.class_names:
+                print("** class doesn't exist **")
+            else:
+                for key in models.storage.all():
+                    key_split = key.split(".")
+                    if key_split[0] == arg:
+                        instance_rep.append(str(models.storage.all()[key]))
+                print(instance_rep)
+        else:
+            for key in models.storage.all():
+                instance_rep.append(str(models.storage.all()[key]))
+            print(instance_rep)
+
+    def do_update(self, arg):
+        """
+        Updates an instance based on the class name and id by adding or
+        updating attribute (save the change into the JSON file)
+        """
+
+        args = arg.split()
+        class_name, id, attr_name = args[0], args[1], args[2]
+        attr_value = args[3]
+
+        if arg is None or arg == "":
+            print("** class name missing **")
+            return
+
+        if class_name not in HBNBCommand.class_names:
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        key = "{}.{}".format(class_name, id)
+        if key not in models.storage.all():
+            print("** no instance found **")
+            return
+        if len(args) < 3:
+            print("** attribute name missing **")
+            return
+        if len(args) < 4:
+            print("** value missing **")
+            return
+
+        instance = models.storage.all()[key]
+        # attr_value = type(attr_name)(attr_value)
+        print(f"{attr_name} is of type {type(attr_value)}")
+        if isinstance(attr_value, str):
+            instance.__dict__[attr_name] = attr_value.replace(
+                '"', '').replace("'", '')
+        instance.save()
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
