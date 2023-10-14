b744363a239af3fafffbb51be515b1ade06f93e0
Hordunlarmy <Hordunlarmy@gmail.com>
2023-10-14 20:26:36 +0100

    Update shebang to use python3 and fix string quotes in __main__ function

--- console.py
+++ console.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python#
+#!/usr/bin/env python3
 """Entry point of the command interpreter"""
 import cmd
 import models
@@ -239,5 +239,5 @@ class HBNBCommand(cmd.Cmd):
             return line
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     HBNBCommand().cmdloop()
